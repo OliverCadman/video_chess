6a84f31df494ee0324265d711a23c387520e4f9a
Oliver Cadman <o.cadman@live.co.uk>
2022-04-15 00:00:49 +0100

    Routed App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, {useState} from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import OnBoard from "./lobby/creategame";
+import JoinRoom from "./lobby/lobby";
+import {ColorContext} from "./context/colorcontext";
+import JoinGame from "./lobby/joingame";
+import ChessGame from "./chess/ui/chessgame";
 
 function App() {
+  const [didRedirect, setDidRedirect] = useState(false);
+
+  const playerDidRedirect = React.useCallback(() => {
+    setDidRedirect(true);
+  }, [])
+
+  const playerDidNotRedirect = React.useCallback(() => {
+    setDidRedirect(false);
+  }, [])
+
+  const [userName, setUserName] = React.useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorContext.Provider
+      value={{
+        didRedirect: didRedirect,
+        playerDidRedirect: playerDidRedirect,
+        playerDidNotRedirect: playerDidNotRedirect,
+      }}
+    >
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={<OnBoard setUserName={setUserName} />}
+          ></Route>
+          <Route
+            path="/game/:gameid"
+            exact
+            element={
+              <>
+                didRedirect ? (
+                <JoinGame userName={userName} isCreator={true} />
+                <ChessGame myUserName={userName} />
+                ) : <JoinRoom />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </ColorContext.Provider>
   );
 }
 
